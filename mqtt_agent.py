dccbdfae1ea9fb29d213f7f32ccfd3280d2600e8
Mark Schneemann <schneebonus@users.noreply.github.com>
2022-06-03 15:06:10 +0200

    Update mqtt_agent.py

--- mqtt_agent.py
+++ mqtt_agent.py
@@ -24,6 +24,14 @@ actions = {
     ("monitor", "on") : "export DISPLAY=:0.0; xrandr --output eDP-1 --mode 1366x768 --pos 3600x312 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --off --output DP-2-2 --mode 1680x1050 --pos 0x0 --rotate normal --output DP-2-3 --primary --mode 1920x1080 --pos 1680x0 --rotate normal",
     ("monitor", "off") : "export DISPLAY=:0.0; xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --off --output DP-2-2 --mode 1680x1050 --pos 0x0 --rotate normal --output DP-2-3 --primary --mode 1920x1080 --pos 1680x0 --rotate normal",
     ("laptop", "off") : "shutdown -t now",
+    
+    # media keys
+    ("media", "pause") : "xdotool key XF86AudioPlay",
+    ("media", "next") : "xdotool key XF86AudioNext",
+    ("media", "last") : "xdotool key XF86AudioPrev",
+    ("media", "mute") : "xdotool key XF86AudioMute",
+    ("media", "lower") : "xdotool key XF86AudioLowerVolume",
+    ("media", "raise") : "xdotool key XF86AudioRaiseVolume",
     }
 
 def on_message(client, userdata, message):
