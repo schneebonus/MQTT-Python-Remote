a68350df7d7a8f12f6e0b861d5d00ddb2fd63180
Mark Schneemann <schneebonus@users.noreply.github.com>
2022-06-03 17:29:31 +0200

    Update mqtt_agent.py

--- mqtt_agent.py
+++ mqtt_agent.py
@@ -23,7 +23,7 @@ actions = {
     # hardware
     ("monitor", "on") : "xrandr --output eDP-1 --mode 1366x768 --pos 3600x312 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --off --output DP-2-2 --mode 1680x1050 --pos 0x0 --rotate normal --output DP-2-3 --primary --mode 1920x1080 --pos 1680x0 --rotate normal",
     ("monitor", "off") : "xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --off --output DP-2-2 --mode 1680x1050 --pos 0x0 --rotate normal --output DP-2-3 --primary --mode 1920x1080 --pos 1680x0 --rotate normal",
-    ("laptop", "off") : "shutdown -t now",
+    ("laptop", "off") : "shutdown now",
     
     # media keys
     ("media", "pause") : "xdotool key XF86AudioPlay",
